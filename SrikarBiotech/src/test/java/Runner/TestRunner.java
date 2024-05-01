f987c9f6e4b9846ebb214241dbe8762f757684a6
Ramya Sree <ramyasree@calibrage.in>
2024-05-01 12:56:47 +0530

    Srikar

--- SrikarBiotech/src/test/java/Runner/TestRunner.java
+++ SrikarBiotech/src/test/java/Runner/TestRunner.java
@@ -12,7 +12,7 @@ import io.cucumber.junit.CucumberOptions;
 				  glue="Stepdefinition",
 				  features = "Features",
 				  monochrome = true, 
-                  tags = "@SBPL_View_Return_Order1")
+                  tags = "@SBPL_View_Return_Order")
 
 
 
